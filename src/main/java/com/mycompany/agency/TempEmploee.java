69c6e9cf2dc82a9627fb7abeb1847528a38254a1
Tommy Jr <rabincrash@gmail.com>
2026-02-11 16:40:40 -0500

    Finished TempEmploee

--- src/main/java/com/mycompany/agency/TempEmploee.java
+++ src/main/java/com/mycompany/agency/TempEmploee.java
@@ -39,7 +39,6 @@ public class TempEmploee extends StaffEmployee {
     //-----------------------------------------------------------------
     // TODO4: Returns information about this hourly employee as a string.
     //-----------------------------------------------------------------
-
     public String toString() {
         String result = super.toString();
         result += "\nCurrent hours: " + hoursWorked;
