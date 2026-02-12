988097daa11b856d9c3e3cdf22f730a52e52875e
aidan <Aidanj711@aol.com>
2026-02-12 16:14:37 -0500

    Made StaffMember class abstract

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -16,7 +16,7 @@ public class Staff
 
         // TODO 1: Add 6 emploees to the staffList
         staffList[0] = new Executive("John", "456 Off Line", "516-666-0101", "516-65-4321", 1000.00);
-        staffList[1] = new StaffMember("Sam", "678 Fifth Ave.", "516-555-0690");
+        staffList[1] = new Intern("Sam", "678 Fifth Ave.", "516-555-0690");
         staffList[2] = new Intern("Alex","987 Wood Blvd","516-555-8374");
         staffList[3] = new TempEmploee("William", "789 Off Rocker", "516-555-000", "632-20-3040", 700.00);
         staffList[4] = new StaffEmployee("Ahmad","321 Elm Lane","516-555-7282", "632-20-3040", 800.00);
--- src/main/java/com/mycompany/agency/StaffMember.java
+++ src/main/java/com/mycompany/agency/StaffMember.java
@@ -3,7 +3,7 @@ package com.mycompany.agency;
 
 
 // TODO 1: Make this class work and public
- public class StaffMember
+ public abstract class StaffMember
 {
     protected String name;
     protected String address;
@@ -25,9 +25,7 @@ package com.mycompany.agency;
     //-----------------------------------------------------------------
     public String toString()
     {
-        System.out.println("Name: " + name);
-        System.out.println("Address: " + address);
-        System.out.println("Phone: " + phone);
+        return"Name: " + name + "\nAddress: " + address +"\nPhone: " + phone;
     }
 
     //-----------------------------------------------------------------
